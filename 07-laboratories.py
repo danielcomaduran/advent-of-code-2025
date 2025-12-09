9d58aadc06efc748157810386fc777144f6011dd
Daniel Comaduran <dcomadur@ucalgary.ca>
2025-12-09 15:12:39 -0700

    day 7

--- 07-laboratories.py
+++ 07-laboratories.py
@@ -1,5 +1,5 @@
 #%% Import data
-with open("example.txt") as file:
+with open("input.txt") as file:
     manifold = file.read()
 
 manifold_lines = manifold.split("\n")
@@ -7,36 +7,24 @@ manifold_lines = manifold.split("\n")
 #%% Functions
 def count_splits(manifold_lines: list, count_timelines:bool = False) -> int:
     beam_splits = 0
-    current_beams = []
-    combinations = 0
-    for l, line in enumerate(manifold_lines):
-        # Detect start of beam
-        if l == 0:
-            for c, char in enumerate(line):
-                if char == "S":
-                    current_beams.append(c)
-                    break
+    beams = [0] * len(manifold_lines[0])
 
-        # Detect beam splits and ends
-        else:
-            row_splits = 0
-            new_beams = []
+    # Find start point
+    start_point = manifold_lines[0].index("S")
+    beams[start_point] = 1
+    timeline_count = 1
 
-            for b, beam in enumerate(current_beams):
-                if line[beam] == "^": # Beam splits
-                    row_splits += 1
-                    new_beams.extend([beam-1, beam+1])
-                else: # Beam continues
-                    new_beams.append(beam)
-                    
-            current_beams = list(set(new_beams))  # Remove duplicates
-            
-            if row_splits > 0:
-                beam_splits += row_splits 
-            combinations += len(current_beams)
+    for line in manifold_lines[1:]:
+        for c in range(len(beams)):
+            if beams[c] > 0 and line[c] == "^":
+                beam_splits += 1
+                timeline_count += beams[c]
+                beams[c-1] += beams[c]
+                beams[c+1] += beams[c]
+                beams[c] = 0
 
     if count_timelines:
-        return combinations + len(current_beams) - 1 # Add remaining beams at the end
+        return timeline_count
     else:
         return beam_splits
 
@@ -47,5 +35,3 @@ print(f"Part 1: Total beam splits: {beam_splits}")
 #%% Part 2: Count total combinations
 total_combinations = count_splits(manifold_lines, count_timelines=True)
 print(f"Part 2: Total combinations: {total_combinations}")
-
-# %%
