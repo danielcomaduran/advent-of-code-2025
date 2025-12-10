a3d0064f54cd580025a3257d354971b5e1552cb8
Daniel <daniel_comaduran@hotmail.com>
2025-12-10 16:55:10 -0700

    day 8 part 1, not working yet

--- 08-playground.py
+++ 08-playground.py
@@ -6,7 +6,7 @@ boxes_int_positions = [[int(number) for number in numbers.split(",")] for number
 
 #%% Functions
 def compute_distance(box1: list, box2: list) -> int:
-    distance = ((box1[0] - box2[0]) ** 2 + (box1[1] - box2[1]) ** 2) ** 0.5
+    distance = (box1[0] - box2[0]) ** 2 + (box1[1] - box2[1]) ** 2 + (box1[2] - box2[2]) ** 2
     return distance
 
 def connect_boxes(boxes_position: list, n_connections: int) -> int:
@@ -14,22 +14,51 @@ def connect_boxes(boxes_position: list, n_connections: int) -> int:
 
     # Compute distances
     for b1, box1 in enumerate(boxes_position):
-        for b2, box2 in enumerate(boxes_position):
-            if box1 != box2:
-                distances.append(((b1, b2), compute_distance(box1, box2)))
+        for b2, box2 in enumerate(boxes_position[b1+1:], start=b1+1):
+            distances.append(((b1, b2), compute_distance(box1, box2)))
     
     # Sort distances
     distances = sorted(distances, key=lambda x: x[1])
 
     # Create connections
     connections = []
-    for i in range(n_connections):
+    i = 0 # Counter for current number of connections
+    while i < n_connections and i < len(distances):
+        box_pair = distances[i][0]
+
+        # Check if box_id exist in any sublist
+        box1_found = False
+        box2_found = False 
+        for sublist in connections:
+            if box_pair[0] in sublist:
+                box1_found = True
+                for sublist2 in connections:
+                    if sublist2 != sublist and box_pair[1] in sublist2:
+                        box2_found = True
+                        break
+
+            elif box_pair[1] in sublist:
+                box2_found = True
+                for sublist2 in connections:
+                    if sublist2 != sublist and box_pair[0] in sublist2:
+                        box1_found = True
+                        break
+
+        # Add connections appropriately
+        if box1_found and box2_found:
+            pass
+        elif box1_found and not box2_found:
+            sublist.append(box_pair[1])
+        elif box2_found and not box1_found:
+            sublist.append(box_pair[0])
+        else:
+            connections.append([box_pair[0], box_pair[1]])
+
+        i += 1
         
-        connections.append(distances[i])
-    
     return distances
 #%% Part 1
-distances = connect_boxes(boxes_int_positions, n_connections=2)
+distances = connect_boxes(boxes_int_positions, n_connections=10)
 
 
 # %%
