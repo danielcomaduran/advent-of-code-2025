d222aae1515aece643027e02d77e0dd36d99d18b
Daniel <daniel_comaduran@hotmail.com>
2025-12-10 21:54:37 -0700

    day 9 part 1

--- 08-playground.py
+++ 08-playground.py
@@ -1,5 +1,5 @@
 #%% Import data
-with open("example.txt") as file:
+with open("input.txt") as file:
     boxes_positions = file.read().split("\n")
 
 boxes_int_positions = [[int(number) for number in numbers.split(",")] for numbers in boxes_positions]
@@ -9,56 +9,60 @@ def compute_distance(box1: list, box2: list) -> int:
     distance = (box1[0] - box2[0]) ** 2 + (box1[1] - box2[1]) ** 2 + (box1[2] - box2[2]) ** 2
     return distance
 
-def connect_boxes(boxes_position: list, n_connections: int) -> int:
+def get_all_distances(boxes_position: list) -> list[tuple[tuple[int, int], int]]:
     distances = []
-
-    # Compute distances
     for b1, box1 in enumerate(boxes_position):
         for b2, box2 in enumerate(boxes_position[b1+1:], start=b1+1):
             distances.append(((b1, b2), compute_distance(box1, box2)))
-    
-    # Sort distances
-    distances = sorted(distances, key=lambda x: x[1])
+
+    distances.sort(key=lambda x: x[1])
+
+    return distances
+
+def connect_boxes(boxes_position: list, n_connections: int) -> list[list[int]]:
+    # Get all distances
+    distances = get_all_distances(boxes_position)
 
     # Create connections
     connections = []
     i = 0 # Counter for current number of connections
     while i < n_connections and i < len(distances):
-        box_pair = distances[i][0]
+        box1, box2 = distances[i][0]
 
-        # Check if box_id exist in any sublist
-        box1_found = False
-        box2_found = False 
+        # Find existing sublists (if any) for each box
+        sublist1 = None
+        sublist2 = None
         for sublist in connections:
-            if box_pair[0] in sublist:
-                box1_found = True
-                for sublist2 in connections:
-                    if sublist2 != sublist and box_pair[1] in sublist2:
-                        box2_found = True
-                        break
+            if box1 in sublist:
+                sublist1 = sublist
+            if box2 in sublist:
+                sublist2 = sublist
+            if sublist1 and sublist2:
+                break
 
-            elif box_pair[1] in sublist:
-                box2_found = True
-                for sublist2 in connections:
-                    if sublist2 != sublist and box_pair[0] in sublist2:
-                        box1_found = True
-                        break
-
-        # Add connections appropriately
-        if box1_found and box2_found:
-            pass
-        elif box1_found and not box2_found:
-            sublist.append(box_pair[1])
-        elif box2_found and not box1_found:
-            sublist.append(box_pair[0])
+        # Add or merge according to what was found
+        if sublist1 and sublist2:
+            if sublist1 is not sublist2:
+                # Merge the two groups so the connection counts
+                sublist1.extend(x for x in sublist2 if x not in sublist1)
+                connections.remove(sublist2)
+            # else already in same group; skip
+        elif sublist1:
+            sublist1.append(box2)
+        elif sublist2:
+            sublist2.append(box1)
         else:
-            connections.append([box_pair[0], box_pair[1]])
+            connections.append([box1, box2])
 
         i += 1
-        
-    return distances
+    
+    connections.sort(key=len, reverse=True)
+    return connections
 #%% Part 1
-distances = connect_boxes(boxes_int_positions, n_connections=10)
+connections = connect_boxes(boxes_int_positions, n_connections=1000)
+multiplication = 1
+for group in connections[:3]:
+    multiplication *= len(group)
+print(f"Part 1 result: {multiplication}")
 
-
-# %%
+# %% Part 2
